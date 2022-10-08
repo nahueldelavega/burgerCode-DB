15e2138a5c14f7fc872461a5d2091362b52e30f5
Angel <ancar1286@hotmail.com>
2022-10-08 19:24:20 -0300

    controladores y rutas modificadas, base conectada

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors')
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
-const users = require('./routes/users');
-const connectDB = require('./db/connectDB');
+const users = require("./routes/users");
+const connectDB = require("./db/connectDB");
 connectDB();
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users', users);
+app.use("/api/users", users);
 
-app.listen(4000,()=>console.log('Servidor funcionando en 4000'));
+app.listen(4000, () => console.log("Servidor funcionando en 4000"));
