3bdfe130fc183379eefae654982d5bc7e8d25aec
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-11 22:33:01 -0300

    rutas, controlador y modelo de request listo

--- models/Request.js
+++ models/Request.js
@@ -1,5 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({});
+const RequestSchema = new Schema({
+  product: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  price:{
+    type: Number,
+    trim: true,
+    required: true
+  },
+  specification:{
+    type: String,
+    trim: true
+  }
+});
 
-module.exports = model("Request", Schema);
+module.exports = model("Request", RequestSchema);
