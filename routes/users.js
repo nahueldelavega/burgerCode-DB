d6c5232cbb37bd0f41c848418485cea89329f092
María Cecilia <95712696+MCeciliaLuna@users.noreply.github.com>
2022-10-12 13:08:54 -0300

    Revert "Request"

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,10 @@ const { Router } = require("express");
 const { addUser, login, auth, getUsers } = require("../controllers/users");
 const verifyAuth = require("../middlewares/verifyAuth");
 const router = Router();
-const { postRequest, getRequests } = require('../controllers/request')
 
 router.post("/", addUser);
 router.post("/login", login);
 router.get("/auth", verifyAuth, auth);
-router.get("/allusers",verifyAuth, getUsers);
-router.post("/postrequest", postRequest);
-router.get("/getrequests", getRequests);
+router.get("/allusers", verifyAuth, getUsers);
 
 module.exports = router;
