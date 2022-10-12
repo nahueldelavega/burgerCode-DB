254e85ef986b9732d14994d23c3a11dab2d22b30
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-12 15:45:03 -0300

    campos de request listos

--- models/Request.js
+++ models/Request.js
@@ -1,19 +1,19 @@
 const { Schema, model } = require("mongoose");
 
 const RequestSchema = new Schema({
-  product: {
+  name: {
     type: String,
     trim: true,
     required: true,
+  },
+  description:{
+    type: String,
+    trim: true
   },
   price:{
     type: Number,
     trim: true,
     required: true
-  },
-  specification:{
-    type: String,
-    trim: true
   }
 });
 
