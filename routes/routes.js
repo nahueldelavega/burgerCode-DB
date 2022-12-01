426d9859bb017f3f09910c3ed16bc9cbaa995068
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-12-01 18:46:53 -0300

    adduser rutas y controladores listos

--- routes/routes.js
+++ routes/routes.js
@@ -5,15 +5,15 @@ const { getMenus, addMenu, findOneMenu } = require("../controllers/menu");
 const verifyAuth = require("../middlewares/verifyAuth");
 const router = Router();
 
-router.post("/", addUser);
+router.post("/adduser", addUser);
 router.post("/login", login);
 router.get("/auth", verifyAuth, auth);
 router.get("/allusers", verifyAuth, getUsers);
-router.post("/addRequest", addRequest);
-router.get("/getRequest", getRequest);
-router.delete("/deleteRequest/:_id", deleteRequest);
-router.get("/getMenus", getMenus);
-router.post("/addMenu", addMenu);
-router.get("/getOneMenu/:_id", findOneMenu);
+router.post("/addrequest", addRequest);
+router.get("/getrequest", getRequest);
+router.delete("/deleterequest/:_id", deleteRequest);
+router.get("/getmenus", getMenus);
+router.post("/addmenu", addMenu);
+router.get("/getonemenu/:_id", findOneMenu);
 
 module.exports = router;
