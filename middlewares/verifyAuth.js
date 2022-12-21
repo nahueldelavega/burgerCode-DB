98540f4f04ef8242541353496533a2a8717fa3fb
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-12-21 19:03:52 -0300

    prueba para deploy

---
+++ middlewares/verifyAuth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const verifyAuth = (req, res, next) => {
+  try {
+    const token = req.header("authorization");
+    console.log(token);
+    if (!token) throw new Error("Credenciales inválidas");
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.id = payload.id;
+    req.email = payload.email;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: error.message });
+  }
+};
+
+module.exports = verifyAuth;
